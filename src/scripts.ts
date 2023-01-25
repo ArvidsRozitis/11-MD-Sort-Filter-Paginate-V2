ecd00dccca7fb5c25816118a6950c132f7d9191b
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-01-25 21:47:08 +0200

    filtered but not working

--- src/scripts.ts
+++ src/scripts.ts
@@ -13,7 +13,6 @@ import { clearTable } from "./assets/modules/clear-table";
 
 
 
-
 const sortBy = document.querySelectorAll('.js-sort')
 let howToSort = 'asc'
 let sortByColumn = 'name'
@@ -43,10 +42,71 @@ setPage.forEach((page) => {
   });
 });
 
+const filterBy = document.querySelectorAll('.js-filter')
+filterBy.forEach((inputfield: HTMLInputElement) => {
+  inputfield.addEventListener('input', (event) => {
+    if (inputfield.value.length < 3) {
+      event.preventDefault();
+  } else{
+    let filterColumn = 'name'
+    console.log(inputfield.value)
+
+    if(inputfield.classList.contains('js-filter-country-name')) {
+      filterColumn = 'name'      
+    } else if(inputfield.classList.contains('js-filter-country-capital')) {
+      filterColumn = 'capital'
+    } else if(inputfield.classList.contains('js-filter-country-currency-name')) {
+      filterColumn = 'currency.name'
+    } else if(inputfield.classList.contains('js-filter-country-language-name')){
+      filterColumn = 'language.name'
+
+    }
+    axios.get<Country[]>(`http://localhost:3004/countries?${filterColumn}_like=${inputfield.value}`).then(({ data }) => {
+      console.log(data)
+    })
+    clearTable()
+    diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
+  }
+  })
+})
+
+//apakšā dēļ mainīgajiem
 const diplayTable = (page: number, rows: number, sortBy: string, sortOrder: string) => {
   axios.get<Country[]>(`http://localhost:3004/countries?_page=${page}&_limit=${rows}&_sort=${sortBy}&_order=${sortOrder}`).then(({ data }) => {
     createContryRow(data)
+
+
+    
   })
 };
-
+//pirmā ielāde
 diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
+
+
+
+
+
+// axios.get<Country[]>(`http://localhost:3004/countries`).then(({ data }) => {
+//     console.log(data.length)
+//     const createPagginator = () => {
+      
+
+//       if ((data.length >= 1) && (data.length >= 4)) {
+//         const pagginator = document.querySelector('.js-paginator');
+//         const button1 = document.createElement('button');
+//         button1.textContent = currentPage.toString()
+//         pagginator.appendChild(button1)
+
+
+//         const button2 = document.createElement('button');
+//         button1.textContent = currentPage.toString()
+//         pagginator.appendChild(button2)
+//       }
+//       }
+//     createPagginator()
+    
+
+    
+//   })
+
+
