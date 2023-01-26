59f05a19d1a8eac5f05580cbc775ab69c458b45a
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-01-26 12:26:58 +0200

    paginator almost done

--- src/assets/modules/create-rows.ts
+++ src/assets/modules/create-rows.ts
@@ -1,55 +1,55 @@
 interface Country {
-    name: string;
+  name: string;
+  code: string;
+  capital: string;
+  region: string;
+  currency: {
     code: string;
-    capital: string;
-    region: string;
-    currency: {
-      code: string;
-      name: string;
-      symbol: string;
-      flag: string;
-      dialling_code: string;
-      isoCode: string;
-    };
-    language: {
-      code: string;
-      name: string;
-    };
+    name: string;
+    symbol: string;
+    flag: string;
+    dialling_code: string;
+    isoCode: string;
+  };
+  language: {
+    code: string;
+    name: string;
+  };
 }
 
 const tableContent = document.querySelector('.js-country-table-content');
 
-const createContryRow= (data: Country[]) => {
-    data.forEach((country, i) => {
-      let rowClass = i % 2 == 0 ? "table__content-row--bg-lightgrey": "table__content-row--bg-white"
-    
-      const row = document.createElement("tr");
-      row.classList.add('table__content-row', rowClass);
-    
-      let cell = document.createElement('th');
-      cell.classList.add('table__content-cell');
-      cell.innerText = country.name;
-      row.appendChild(cell);
+const createCountryRow= (data: Country[]) => {
+  data.forEach((country, i) => {
+    let rowClass = i % 2 == 0 ? "table__content-row--bg-lightgrey": "table__content-row--bg-white"
+  
+    const row = document.createElement("tr");
+    row.classList.add('table__content-row', rowClass);
+  
+    let cell = document.createElement('th');
+    cell.classList.add('table__content-cell');
+    cell.innerText = country.name;
+    row.appendChild(cell);
 
-      cell = document.createElement('th');
-      cell.classList.add('table__content-cell');
-      cell.innerText = country.capital;
-      row.appendChild(cell);
+    cell = document.createElement('th');
+    cell.classList.add('table__content-cell');
+    cell.innerText = country.capital;
+    row.appendChild(cell);
 
-      cell = document.createElement('th');
-      cell.classList.add('table__content-cell');
-      cell.innerText = country.currency.name;
-      row.appendChild(cell);
+    cell = document.createElement('th');
+    cell.classList.add('table__content-cell');
+    cell.innerText = country.currency.name;
+    row.appendChild(cell);
 
-      cell = document.createElement('th');
-      cell.classList.add('table__content-cell');
-      cell.innerText = country.language.name;
-      row.appendChild(cell);
+    cell = document.createElement('th');
+    cell.classList.add('table__content-cell');
+    cell.innerText = country.language.name;
+    row.appendChild(cell);
 
-      tableContent.appendChild(row)
-    })
-  }
+    tableContent.appendChild(row)
+  })
+}
 
-  export {createContryRow}
+export {createCountryRow}
   
   
--- src/scripts.ts
+++ src/scripts.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 import { Country } from "./assets/modules/interfaces";
-import { createContryRow } from "./assets/modules/create-rows";
+import { createCountryRow } from "./assets/modules/create-rows";
 import { clearTable } from "./assets/modules/clear-table";
 
 // 1 = kā importētinterfaces iekš moduļa
 
 
 
-
-//------------kārtošana
+//----globālie pagaidām
 const sortBy = document.querySelectorAll('.js-sort')
 let howToSort = 'asc'
 let sortByColumn = 'name'
+let rowsOnPage = 20;
+let currentPage = 1;
 
+//------------kārtošana
 sortBy.forEach((button) => {
   button.addEventListener('click', () => {
     sortByColumn = button.id
@@ -26,17 +28,6 @@ sortBy.forEach((button) => {
     diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
   })
 })
-//------------lapas izvēle
-const setPage = document.querySelectorAll(".js-page");
-let rowsOnPage = 20;
-let currentPage = 1;
-setPage.forEach((page) => {
-  page.addEventListener("click", () => {
-    currentPage = Number(page.textContent);
-    clearTable()
-    diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
-  });
-});
 
 //------------filtrs
 const filterBy = document.querySelectorAll('.js-filter')
@@ -66,56 +57,113 @@ filterBy.forEach((inputfield: HTMLInputElement) => {
       axios.get<Country[]>(`http://localhost:3004/countries?${filterColumn}_like=${inputfield.value}`).then(({ data }) => {
         console.log(data)
         clearTable()
-        createContryRow(data)   
+        createCountryRow(data)   
       })
     }
   })
 })
 
-
 const createPagginator = (page: number, dataLenght: number) => {
+  let rowsOnPage = 20;
   const pageCount = Math.ceil(dataLenght/20);//data lenght /20 math
 
-  if(pageCount - page > 1) {
-  const pagintaorWrapper = document.querySelector('.js-paginator');
-  const buttonFirst = document.createElement('button');
-  buttonFirst.classList.add('js-page');
-  buttonFirst.textContent = currentPage.toString()
-  pagintaorWrapper.appendChild(buttonFirst)
-  
-  const buttonSecound = document.createElement('button');
-  buttonSecound.classList.add('js-page');
-  buttonSecound.textContent = String(currentPage +1 );
-  pagintaorWrapper.appendChild(buttonSecound)
-  
-  const buttonThereIsMorePages = document.createElement('button');
-  buttonThereIsMorePages.classList.add('js-page');
-  buttonThereIsMorePages.textContent = '...'
-  pagintaorWrapper.appendChild(buttonThereIsMorePages)
-  
-  const buttonlast = document.createElement('button');
-  buttonlast.classList.add('js-page');
-  buttonlast.textContent = pageCount.toString()
-  pagintaorWrapper.appendChild(buttonlast)
-  }
+  if(pageCount - page > 2) {
+    const pagintaorWrapper = document.querySelector('.js-paginator');
+    pagintaorWrapper.innerHTML= ''
+
+    const buttonFirst = document.createElement('button');
+    buttonFirst.classList.add('table__page-button');
+    buttonFirst.classList.add('js-page');
+    buttonFirst.classList.add('table__page-button--active');
+    buttonFirst.textContent = currentPage.toString()
+    pagintaorWrapper.appendChild(buttonFirst)
+    
+    const buttonSecound = document.createElement('button');
+    buttonSecound.classList.add('table__page-button');
+    buttonSecound.classList.add('js-page');
+    buttonSecound.textContent = String(currentPage +1 );
+    pagintaorWrapper.appendChild(buttonSecound)
+    
+    const buttonThereIsMorePages = document.createElement('button');
+    buttonThereIsMorePages.classList.add('table__page-button');
+    buttonThereIsMorePages.classList.add('js-page');
+    buttonThereIsMorePages.textContent = '...'
+    buttonThereIsMorePages.setAttribute('disabled', 'disabled')
+    pagintaorWrapper.appendChild(buttonThereIsMorePages)
+    
+    const buttonlast = document.createElement('button');
+    buttonlast.classList.add('table__page-button');
+    buttonlast.classList.add('js-page');
+    buttonlast.textContent = pageCount.toString()
+    pagintaorWrapper.appendChild(buttonlast)
+    
+    //----------------jāizness funkcijā!!!
+    const setPage = document.querySelectorAll(".js-page");
+    setPage.forEach((page) => {
+      page.addEventListener("click", () => {
+      currentPage = Number(page.textContent);
+      clearTable()
+      diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
+      });
+    });
+
+  } else if (pageCount - page < 4) {
+    const pagintaorWrapper = document.querySelector('.js-paginator');    
+    pagintaorWrapper.innerHTML= ''
+
+    const buttonOneback = document.createElement('button');    
+    buttonOneback.classList.add('table__page-button');
+    buttonOneback.classList.add('js-page');
+    buttonOneback.textContent = String(pageCount -3 );
+    pagintaorWrapper.appendChild(buttonOneback)
+
+    const buttonFirst = document.createElement('button');
+    buttonFirst.classList.add('table__page-button');
+    buttonFirst.classList.add('js-page');
+    buttonFirst.textContent = String(pageCount -2 );
+    pagintaorWrapper.appendChild(buttonFirst)
+    
+    const buttonSecound = document.createElement('button');
+    buttonSecound.classList.add('table__page-button');
+    buttonSecound.classList.add('js-page');
+    buttonSecound.textContent = String(pageCount -1 );
+    pagintaorWrapper.appendChild(buttonSecound)
+
+    const buttonlast = document.createElement('button');
+    buttonlast.classList.add('table__page-button');
+    buttonlast.classList.add('js-page');
+    buttonlast.textContent = pageCount.toString()
+    pagintaorWrapper.appendChild(buttonlast)    
+
+    //----------------jāizness funkcijā!!!
+    const setPage = document.querySelectorAll(".js-page");
+    setPage.forEach((page) => {
+      page.addEventListener("click", () => {
+        currentPage = Number(page.textContent);
+        clearTable()
+        diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
+        
+        console.log(page)
+      });
+    });
+  } 
+
+
+
+
+
 }
 
 //------------------lapas ielāde
 const diplayTable = (page: number, rows: number, sortBy: string, sortOrder: string) => {
   axios.get<Country[]>(`http://localhost:3004/countries?_page=${page}&_limit=${rows}&_sort=${sortBy}&_order=${sortOrder}`).then(({ data }) => {
-    createContryRow(data)
+    createCountryRow(data)
   })
   axios<Country[]>(`http://localhost:3004/countries`).then(({ data }) => {
     const dataLengt = data.length
     createPagginator(page, dataLengt)  
   })
 };
+
 //pirmā ielāde
 diplayTable(currentPage, rowsOnPage, sortByColumn, howToSort);
-
-
-
-
-
-
-
